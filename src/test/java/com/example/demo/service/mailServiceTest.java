340e3f8fc0b3f54042e9a7b780982f70e9fb2ee4
natsutonbi <1306512118@qq.com>
2023-05-31 17:37:44 +0800

    删除mailtest

--- src/test/java/com/example/demo/service/mailServiceTest.java
+++ src/test/java/com/example/demo/service/mailServiceTest.java
@@ -1,17 +1,17 @@
 package com.example.demo.service;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import com.example.demo.user.service.mailService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class mailServiceTest {
     private String destMail="1306512118@qq.com";
     
