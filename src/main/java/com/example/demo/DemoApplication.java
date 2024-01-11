918ef716a805bf94f6191b8f563f43f44727afa8
natsutonbi <1306512118@qq.com>
2024-01-12 02:36:47 +0800

    随便什么更改，测试流水线

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 
